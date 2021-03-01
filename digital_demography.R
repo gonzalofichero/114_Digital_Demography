f831905cc5bfdf55611b3cf563db59a6f617ab40
Gonzalo <gonzalo.fce@gmail.com>
2021-03-01 19:14:51 +0100

    add tagging add missing Google data frame add missing plot for regions

--- digital_demography.R
+++ digital_demography.R
@@ -1,26 +1,24 @@
-# Loading libraries
+#### Loading libraries ####
 library(tidyverse)
 library(lubridate)
 library(data.table)
 
 
-# Loading Regions from UK to do inner join of data:
+#### Loading Regions from UK to do inner join of data: ####
 uk_nations <- read_csv("uk_nations.csv")
 
 uk_regions <- read_csv("uk_regions.csv")
 
 uk_geo <- uk_regions %>% left_join(uk_nations, by = "la")
 
-rm(uk_nations)
-rm(uk_regions)
 
-
-
-# Loading data from Apple:
+#### Apple ####
+##### Importing #####
 apple <- read_csv("applemobilitytrends-2021-02-27.csv")
 
 glimpse(apple)
 
+##### Pipe transformation #####
 apple %>% 
   filter(region %in% c("England", "Wales")) %>%
   pivot_longer(cols = starts_with("202"), names_to = "day", values_to = "mobility") %>% 
@@ -32,11 +30,13 @@ apple %>%
 
 
 
-# Loading data from Google:
+#### Google ####
+##### Importing #####
 google <- read_csv("Global_Mobility_Report.csv")
 
 glimpse(google)
 
+##### Pipe transformation #####
 google %>% 
   filter(country_region == "United Kingdom") %>%
   rename(la = sub_region_1,
@@ -55,19 +55,22 @@ google %>%
   pivot_longer(cols = c("retail", "grocery", "parks", "transit", "workplaces", "residential"), 
                names_to = "type", 
                values_to = "mobility") %>% 
-  mutate(week = epiweek(date)) -> google_v2
+  mutate(week = epiweek(date)) %>% 
+  mutate(mobility = case_when(type == "residential" ~ mobility * (-1),
+                              TRUE ~ mobility)) -> google_v2
   
   
 glimpse(google_v2)
 
 
 
-# Loading data from Facebook:
+#### Facebook ####
+##### Importing #####
 facebook <- fread("movement-range-2021-02-27.txt")
 
 glimpse(facebook)
 
-
+##### Pipe transformation #####
 facebook %>% 
   filter(polygon_name %in% c("England", "Wales")) %>% 
   mutate(date = ymd(ds)) %>% 
@@ -79,9 +82,10 @@ facebook %>%
 glimpse(facebook_v2)
 
 
+#### Exercise 1: Plotting ####
 # Let's plot to see what we have in each data.frame
 
-# Apple:
+##### Apple #####
 apple_v2 %>% 
   filter(year(day) == 2020, week <= 33) %>% 
   group_by(region, week, transportation_type) %>% 
@@ -90,21 +94,17 @@ apple_v2 %>%
   facet_grid(~region)
 
 
-# Google:
+##### Google #####
 google_v2 %>% 
   filter(year(date) == 2020, week <= 33,
          nation %in% c("england", "wales")) %>% 
-  mutate(mobility_fix = case_when(type == "residential" ~ mobility * (-1),
-                              TRUE ~ mobility)) %>% 
   group_by(nation, week, type) %>% 
   summarise(mean_mob = mean(mobility_fix, na.rm = T)) %>% 
   ggplot(aes(x = week, y = mean_mob, color = type)) + geom_point() +
   facet_grid(~nation)
 
-# Have to create a sumup column for each type to have aggregate data
 
-
-# Facebook:
+##### Facebook #####
 facebook_v2 %>% 
   filter(year(date) == 2020, week <= 33,
          polygon_name %in% c("England", "Wales")) %>%
@@ -114,5 +114,48 @@ facebook_v2 %>%
   facet_grid(~polygon_name)
   
   
-  
+
+
+#### Exercise 2: Missing values in Google ####
+
+# Only Google data
+# All data, also the missing stuff
+google %>% 
+  filter(country_region == "United Kingdom") %>%
+  rename(la = sub_region_1,
+         retail = retail_and_recreation_percent_change_from_baseline,
+         grocery = grocery_and_pharmacy_percent_change_from_baseline,
+         parks = parks_percent_change_from_baseline,
+         transit = transit_stations_percent_change_from_baseline,
+         workplaces = workplaces_percent_change_from_baseline,
+         residential = residential_percent_change_from_baseline
+  ) %>%
+  mutate(la = tolower(la)) %>%
+  ### Using uk_regions for joining this time, since we don't care for Nations variable
+  left_join(uk_regions, by = "la") %>% 
+  ###
+  select(country_region, la, region, date,
+         retail, grocery, parks, transit, workplaces, residential) %>% 
+  pivot_longer(cols = c("retail", "grocery", "parks", "transit", "workplaces", "residential"), 
+               names_to = "type", 
+               values_to = "mobility") %>% 
+  mutate(week = epiweek(date)) %>% 
+  filter(year(date) == 2020, week <= 33) %>% 
+  mutate(mobility = case_when(type == "residential" ~ mobility * (-1),
+                              TRUE ~ mobility),
+         miss_flag = case_when(is.na(mobility) == TRUE ~ 1,
+                               TRUE ~ 0)) %>%
+  group_by(region, type, week) %>% 
+  summarise(share_miss = sum(miss_flag),
+            obs = n()) -> google_miss
+
+
+google_miss %>% 
+  filter(region %in% c("east of england", "east midlands", "london",
+                       "north east england", "north west england", "south east england",
+                       "south west england", "wales", "west midlands", "yorkshire and the humber")) %>% 
+  ggplot(aes(x = week, y = share_miss/obs, color = type)) + geom_point() +
+  facet_wrap(~region, nrow = 3)
+
+
 
